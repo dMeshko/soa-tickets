6b7f838b499a8969c4d4b5efef94cc93777bb0bb
Angjel Kichukov <angelkicukov@hotmail.com>
2017-11-01 17:57:02 +0100

    event service complete

--- src/main/java/finki/ukim/mk/soatickets/business/services/implementation/EventService.java
+++ src/main/java/finki/ukim/mk/soatickets/business/services/implementation/EventService.java
@@ -4,35 +4,75 @@ import finki.ukim.mk.soatickets.business.services.IEventService;
 import finki.ukim.mk.soatickets.business.view.models.events.CreateEventViewModel;
 import finki.ukim.mk.soatickets.business.view.models.events.EventViewModel;
 import finki.ukim.mk.soatickets.business.view.models.events.UpdateEventViewModel;
+import finki.ukim.mk.soatickets.models.events.Event;
+import finki.ukim.mk.soatickets.repositories.IEventRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Service
 public class EventService implements IEventService {
+
+    @Autowired
+    private IEventRepository eventRepository;
+
+    private ModelMapper modelMapper;
+
+    public EventService() { modelMapper = new ModelMapper(); }
 
     @Override
     public List<EventViewModel> getAll() {
-        return null;
+        List<EventViewModel> result = new ArrayList<>();
+        for (Event event : eventRepository.findAll())
+            result.add(modelMapper.map(event, EventViewModel.class));
+
+        return result;
     }
 
     @Override
     public EventViewModel getById(Long eventId) throws Exception {
-        return null;
+        Event result = eventRepository.findOne(eventId);
+
+        if(result == null) {
+            throw new Exception("There is no such event!");
+        }
+        return modelMapper.map(result, EventViewModel.class);
     }
 
     @Override
     public Long register(CreateEventViewModel event) {
-        return null;
+        Event eventDbo = new Event(event.getOwnerId(),
+                                   event.getName(),
+                                   event.getDescrption(),
+                                   event.getLocation(),
+                                   event.getDate());
+        return eventRepository.save(eventDbo).getId();
     }
 
     @Override
     public Long update(UpdateEventViewModel event) {
-        return null;
+        Event dboEvent = eventRepository.findOne(event.getId());
+        dboEvent.setName(event.getName());
+        dboEvent.setDescrption(event.getDescrption());
+        dboEvent.setLocation(event.getLocation());
+        dboEvent.setDate(event.getDate());
+
+        return eventRepository.save(dboEvent).getId();
     }
 
     @Override
     public Long delete(Long eventId) throws Exception {
-        return null;
+        Event dboEvent = eventRepository.findOne(eventId);
+        if ( dboEvent == null) {
+            throw new Exception("No such Event!");
+        }
+        eventRepository.delete(dboEvent);
+
+        return dboEvent.getId();
     }
 
     @Override
--- src/main/java/finki/ukim/mk/soatickets/repositories/IEventRepository.java
+++ src/main/java/finki/ukim/mk/soatickets/repositories/IEventRepository.java
@@ -1,10 +1,11 @@
 package finki.ukim.mk.soatickets.repositories;
 
 import finki.ukim.mk.soatickets.models.events.Event;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Date;
 
-public interface IEventRepository {
+public interface IEventRepository extends CrudRepository<Event, Long> {
     Event findByName(String name);
     Event findByLocation(String location);
     Event findByDate(Date date);
