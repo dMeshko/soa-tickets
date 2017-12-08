3ed97651c98fb8741a16c62fe9fe396a893ab309
Darko Meshkovski <darkom@workflowstoday.com>
2017-12-08 22:11:20 +0100

    notifications display fix.

--- src/main/java/finki/ukim/mk/soatickets/business/services/implementation/NotificationService.java
+++ src/main/java/finki/ukim/mk/soatickets/business/services/implementation/NotificationService.java
@@ -3,6 +3,7 @@ package finki.ukim.mk.soatickets.business.services.implementation;
 import finki.ukim.mk.soatickets.business.services.INotificationService;
 import finki.ukim.mk.soatickets.business.view.models.LookupViewModel;
 import finki.ukim.mk.soatickets.business.view.models.blog.PostViewModel;
+import finki.ukim.mk.soatickets.business.view.models.user.NotificationType;
 import finki.ukim.mk.soatickets.business.view.models.user.NotificationViewModel;
 import finki.ukim.mk.soatickets.models.blog.Post;
 import finki.ukim.mk.soatickets.models.user.Notification;
@@ -13,7 +14,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -40,15 +44,49 @@ public class NotificationService implements INotificationService {
         if (user == null)
             throw new Exception("User not found!");
 
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
         List<NotificationViewModel> result = new ArrayList<>();
         for (Notification notification : user.getReceivedNotifications())
         {
             NotificationViewModel notificationViewModel = modelMapper.map(notification, NotificationViewModel.class);
             LookupViewModel<Long> userFromViewModel = new LookupViewModel<>(notification.getUserFrom().getId(), notification.getUserFrom().toString());
             notificationViewModel.setUserFrom(userFromViewModel);
+            notificationViewModel.setCreatedAt(dateFormat.format(notification.getCreatedAt()));
             result.add(notificationViewModel);
         }
 
         return result;
+    }
+
+    @Override
+    public long sendNotification(long userFromId, long userToId, NotificationType notificationType) throws Exception {
+        User userFrom = userRepository.findOne(userFromId);
+        if (userFrom == null)
+            throw new Exception("Receiver not found!");
+
+        User userTo = userRepository.findOne(userToId);
+        if (userTo == null)
+            throw new Exception("Sender not found!");
+
+        Date date = new Date();
+
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+        String notificationContent = resolveNotificationContent(notificationType);
+        String parsedNotificationContent = notificationContent.replace("{0}", userFrom.toString());
+
+        Notification notification = new Notification(notificationContent, userFrom, userTo);
+        notificationRepository.save(notification);
+
+        return notification.getId();
+    }
+
+    private String resolveNotificationContent(NotificationType notificationType) throws Exception {
+        switch (notificationType){
+            case Comment: return "{0} commented on your post";
+            case Message: return "{0} sent you a message";
+            default: throw new Exception("Invalid notification type!");
+        }
     }
 }
