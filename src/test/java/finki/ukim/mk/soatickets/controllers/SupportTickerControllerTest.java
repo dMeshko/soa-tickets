7ec46d4f39eedae26b97e57c2f63b19670465c88
Angjel Kichukov <angelkicukov@hotmail.com>
2018-01-30 13:51:41 +0100

    Support Park Tickets

--- src/test/java/finki/ukim/mk/soatickets/controllers/SupportTickerControllerTest.java
+++ src/test/java/finki/ukim/mk/soatickets/controllers/SupportTickerControllerTest.java
@@ -1,21 +1,27 @@
 package finki.ukim.mk.soatickets.controllers;
 
 import finki.ukim.mk.soatickets.business.services.ISupportTicketService;
+import finki.ukim.mk.soatickets.business.services.IUsersService;
+import finki.ukim.mk.soatickets.business.view.models.user.UserViewModel;
 import finki.ukim.mk.soatickets.repositories.ISupportTicketRepository;
 import org.json.JSONObject;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.security.Principal;
 import java.util.Date;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -35,11 +41,29 @@ public class SupportTickerControllerTest {
     @Mock
     ISupportTicketRepository mockSupportTicketRepository;
 
+    @Mock
+    IUsersService mockUserService;
+
     MockMvc mockMvc;
 
     @Before
-    public void setup() {
+    public void setup() throws Exception {
+        UserViewModel userViewModel = new UserViewModel();
+        userViewModel.setId(Long.valueOf(1));
+        userViewModel.setActive(true);
+        userViewModel.setFirstName("Test");
+        userViewModel.setLastName("Tester");
+        userViewModel.setEmail("spring@test.mk");
+        userViewModel.setPhoneNumber("123456789");
+
+        Mockito.when(mockUserService.findByEmail("user")).thenReturn(userViewModel);
+        Mockito.when(mockUserService.findByUsername("user")).thenReturn(userViewModel);
+        Mockito.when(mockSupportTicketService.createSupportTicket(any(Long.class), any(String.class))).thenReturn(Long.valueOf(1));
+
         mockMvc = MockMvcBuilders.standaloneSetup(mockSupportTicketController).build();
+    }
+
+    private void when(IUsersService mockUserService) {
     }
 
     @Test
@@ -55,7 +79,18 @@ public class SupportTickerControllerTest {
                                            .put("user", "1")
                                            .toString();
 
-        mockMvc.perform(post("/api/support/").accept(MediaType.APPLICATION_JSON).content(contetnts)).andExpect(status().isOk());
+        Principal principal = new Principal() {
+            @Override
+            public String getName() {
+                return "user";
+            }
+        };
+
+        mockMvc.perform(post("/api/support/").accept(MediaType.APPLICATION_JSON)
+                                                        .content(contetnts)
+                                                        .param("content", "This")
+                                                        .principal(principal))
+                                                        .andExpect(status().isOk()).andReturn();
     }
 
 }
